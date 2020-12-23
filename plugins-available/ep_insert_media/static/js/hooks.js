8ad3fbafebf2a3aae1861d83b64c8d5ff09e4c4b
Clément FORGEARD <clement_forgeard@outlook.fr>
2020-12-23 17:27:05 +0000

    [ep_insert_media] remove all features except insert remote image + format code

--- plugins-available/ep_insert_media/static/js/hooks.js
+++ plugins-available/ep_insert_media/static/js/hooks.js
@@ -1,147 +1,44 @@
 var randomString = require('ep_etherpad-lite/static/js/pad_utils').randomString;
+var $ = require('ep_etherpad-lite/static/js/rjquery').$;
 
-exports.aceInitInnerdocbodyHead = function(hook_name, args, cb) {
+exports.aceInitInnerdocbodyHead = function (hook_name, args, cb) {
   args.iframeHTML.push('<link rel="stylesheet" type="text/css" href="/static/plugins/ep_insert_media/static/css/ace.css"/>');
   return cb();
 };
 
-exports.aceAttribsToClasses = function(hook_name, args, cb) {
-  console.log("aceAttribsToClasses",args)
-  if (args.key == 'embedMedia' && args.value != "")
-    return cb(["embedMedia:" + args.value]);
-  if (args.key == 'insertEmbedPicture' && args.value != "")
-  return cb(["insertEmbedPicture:" + args.value]);
-  if (args.key == 'insertEmbedVideo' && args.value != "")
-  return cb(["insertEmbedVideo:" + args.value]);
-  if (args.key == 'insertEmbedAudio' && args.value != "")
-  return cb(["insertEmbedAudio:" + args.value]);
-  // if (args.key == 'insertEmbedPicture' && args.value == "embedRemoteImageSpanLarge")
-  // return cb(["insertEmbedPictureBig:" + args.value]);
+exports.aceAttribsToClasses = function (hook_name, args, cb) {
+  if (args.key === 'insertEmbedPicture' && args.value !== '') {
+    return cb(["insertEmbedPicture:" + args.value]);
+  }
+  return cb([]);
 };
 
-exports.aceCreateDomLine = function(hook_name, args, cb) {
-  if (args.cls.indexOf('embedMedia:') >= 0) {
-    var clss = [];
-    var argClss = args.cls.split(" ");
-     var value;
-
-    for (var i = 0; i < argClss.length; i++) {
-      var cls = argClss[i];
-      if (cls.indexOf("embedMedia:") != -1) {
-	      value = cls.substr(cls.indexOf(":")+1);
-      } else {
-	      clss.push(cls);
-      }
-    }
-    try{
-      var mediaData = JSON.parse(value)
-    } catch(e) {
-      console.log(e)
-    }
-    if (mediaData){
-      var height= "175"
-      if(mediaData.size =="Medium"){
-        height= "350"
-      }
-      if(mediaData.size =="Large"){
-        height= "540"
-      }
-      
-      return cb([{cls: clss.join(" "), extraOpenTags: "<span style='height:"+height+"px' class='embedMedia'><span class='media'>" + exports.cleanEmbedCode(unescape(mediaData.url),mediaData) + "</span><span class='character'>", extraCloseTags: '</span>'}]);
-    }
-    
-  }
+exports.aceCreateDomLine = function (hook_name, args, cb) {
   //--------------------- insertEmbedPicture
   if (args.cls.indexOf('insertEmbedPicture:') >= 0) {
-      var clss = [];
-      var argClss = args.cls.split(" ");
-      var value;
-      for (var i = 0; i < argClss.length; i++) {
-        var cls = argClss[i];
-        if (cls.indexOf("insertEmbedPicture:") != -1) {
-          value = cls.substr(cls.indexOf(":")+1);
-        }
-        else {
-          clss.push(cls);
-        }
-      }
-      console.log(value)
-      try{
-        var mediaData = JSON.parse(value)
-      } catch(e) {
-        console.log(e)
-      }
-      if(mediaData)
-        return cb([{cls: clss.join(" "), extraOpenTags: "<span data-size='"+mediaData.size+"' data-align='"+mediaData.align+"' data-url='"+unescape(mediaData.url)+"' id='emb_img-"+randomString(16)+"' class='embedRemoteImageSpan ep_insert_media_"+mediaData.size+" ep_insert_media_"+mediaData.align+"'><span class='image'>" + exports.cleanEmbedPictureCode(unescape(mediaData.url)) + "</span><span class='character'>", extraCloseTags: '</span>'}]);
-
-  }
-
-
-  ////////////////////////////////// insertEmbedVideo
-  if (args.cls.indexOf('insertEmbedVideo:') >= 0) {
     var clss = [];
     var argClss = args.cls.split(" ");
     var value;
-
     for (var i = 0; i < argClss.length; i++) {
       var cls = argClss[i];
-      if (cls.indexOf("insertEmbedVideo:") != -1) {
-        value = cls.substr(cls.indexOf(":")+1);
+      if (cls.indexOf("insertEmbedPicture:") != -1) {
+        value = cls.substr(cls.indexOf(":") + 1);
       }
       else {
         clss.push(cls);
       }
     }
-    try{
+    console.log(value)
+    try {
       var mediaData = JSON.parse(value)
-    } catch(e) {
+    } catch (e) {
       console.log(e)
     }
-    if(mediaData)
-      return cb([{cls: clss.join(" "), extraOpenTags: "<span data-url='"+unescape(mediaData.url)+"' id='emb_video-"+randomString(16)+"' class='embedRemoteVideoSpan'><span class='video'>" + exports.cleanEmbedVideoCode(unescape(mediaData.url),mediaData) + "</span><span class='character'>", extraCloseTags: '</span>'}]);
-
+    if (mediaData)
+      return cb([{ cls: clss.join(" "), extraOpenTags: "<span data-size='" + mediaData.size + "' data-align='" + mediaData.align + "' data-url='" + unescape(mediaData.url) + "' id='emb_img-" + randomString(16) + "' class='embedRemoteImageSpan ep_insert_media_" + mediaData.size + " ep_insert_media_" + mediaData.align + "'><span class='image'>" + exports.cleanEmbedPictureCode(unescape(mediaData.url)) + "</span><span class='character'>", extraCloseTags: '</span>' }]);
   }
-
-
-
-
-  ////////////////////////////////// insertEmbedAudio
-  if (args.cls.indexOf('insertEmbedAudio:') >= 0) {
-    var clss = [];
-    var argClss = args.cls.split(" ");
-    var value;
-
-    for (var i = 0; i < argClss.length; i++) {
-      var cls = argClss[i];
-      if (cls.indexOf("insertEmbedAudio:") != -1) {
-        value = cls.substr(cls.indexOf(":")+1);
-      }
-      else {
-        clss.push(cls);
-      }
-    }
-    try{
-      var mediaData = JSON.parse(value)
-    } catch(e) {
-      console.log(e)
-    }
-
-    if (mediaData){
-      return cb([{cls: clss.join(" "), extraOpenTags: "<span data-url='"+unescape(mediaData.url)+"' id='emb_audio-"+randomString(16)+"' class='embedRemoteAudioSpan'><span class='audio'>" + exports.cleanEmbedAudioCode(unescape(mediaData.url),mediaData) + "</span><span class='character'>", extraCloseTags: '</span>'}]);
-
-    }
-
-  }
-
   return cb();
 };
-
-
-var wrap = function (obj) {
-  var wrapper = $("<div></div>");
-  wrapper.append(obj);
-  return wrapper;
-}
 
 var filter = function (node) {
   node = $(node);
@@ -153,119 +50,28 @@ var filter = function (node) {
   }
 }
 
-var parseUrlParams = function (url) {
-  var res = {};
-  url.split("?")[1].split("&").map(function (item) {
-    item = item.split("=");
-    res[item[0]] = item[1];
-  });
-  return res;
-}
-
-exports.sanitize = function (inputHtml) {
-  // Monkeypatch the sanitizer a bit, adding support for embed tags and fixing broken param tags
-
-  html4.ELEMENTS.embed = html4.eflags.UNSAFE;
-  html4.ELEMENTS.param = html4.eflags.UNSAFE; // NOT empty or we break stuff in some browsers...
-
-  return html.sanitizeWithPolicy(inputHtml, function(tagName, attribs) {
-    if ($.inArray(tagName, ["embed", "object", "iframe", "param","video"]) == -1) {
-      return null;
-    }
-    return attribs;
-  });
-}
-
-exports.cleanEmbedAudioCode =  function(orig,mediaData) {
+exports.cleanEmbedPictureCode = function (orig) {
   var value = $.trim(orig);
-  return '<audio class="audioClass ep_insert_media_'+mediaData.size+' ep_insert_media_'+mediaData.align+'" controls><source src="'+value+'" type="audio/mpeg"></video>';
-}
-exports.cleanEmbedVideoCode =  function(orig,mediaData) {
-  var value = $.trim(orig);
-  return '<video class="videoClass ep_insert_media_'+mediaData.size+' ep_insert_media_'+mediaData.align+'" controls><source src="'+value+'" type="video/mp4"></video>';
-}
-exports.cleanEmbedPictureCode = function(orig) {
-  var value = $.trim(orig);
-  return "<img class='embedRemoteImage' src='"+value+"'>";
+  return "<img class='embedRemoteImage' src='" + value + "'>";
 }
 
-
-exports.cleanEmbedCode = function (orig,mediaData) {
-  var res = null;
-
-  value = $.trim(orig);
-
-  if (value.indexOf('http://') == 0 || value.indexOf('https://') == 0) {
-    if (value.indexOf("www.youtube.com") != -1) {
-      var video = escape(parseUrlParams(value).v);
-      res = '<iframe  type="text/html"   class="video ep_insert_media_'+mediaData.size+' ep_insert_media_'+mediaData.align+'"  src="https://www.youtube.com/embed/' + video + '?enablejsapi=1&origin=https://docs.plus" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-    } else if (value.indexOf("vimeo.com") != -1) {
-      var video = escape(value.split("/").pop());
-      res = '<iframe   class="video" src="http://player.vimeo.com/video/' + video + '?color=ffffff" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
-    } else {
-      console.warn("Unsupported embed url: " + orig);
-    }
-  } else if (value.indexOf('<') == 0) {
-    value = $.trim(exports.sanitize(value));
-    if (value != '') {
-        console.log([orig, value]);
-      res = value;
-    } else {
-      console.warn("Invalid embed code: " + orig);
-    }
-  } else {
-    console.warn("Invalid embed code: " + orig);
-  }
-
-  if (!res) {
-    return  "<img src='/static/plugins/ep_insert_media/static/html/invalid.png'>";
-  }
-
-  return res;
-}
-
-
-exports.aceInitialized = function(hook, context){
+exports.aceInitialized = function (hook, context, cb) {
   var padOuter = $('iframe[name="ace_outer"]').contents();
   var padInner = padOuter.find('iframe[name="ace_inner"]');
   var padeditor = require('ep_etherpad-lite/static/js/pad_editor').padeditor;
 
-  // padInner.contents().on("click", ".embedRemoteImageSpanLarge", function(e){
-  //   var url = $(this).data("url")
-  //   var id = $(this).attr("id")
-  //   var selector = "#"+id
-  //   var ace = padeditor.ace;
+  padInner.contents().on("click", ".embedRemoteImageSpan", function (e) {
+    var url = $(this).data("url")
+    var id = $(this).attr("id")
+    var selector = "#" + id
+    var ace = padeditor.ace;
 
-  //   padeditor.ace.callWithAce(function (aceTop) {
-  //     var repArr = aceTop.ace_getRepFromSelector(selector, padInner);
-  //     $.each(repArr, function(index, rep){
-  //       ace.callWithAce(function (ace){
-  //         ace.ace_performSelectionChange(rep[0],rep[1],true);
-  //         ace.ace_setAttributeOnSelection('insertEmbedPicture', url);
-  //       });
-  //     });
-  //   }, "changeEmbedPicture");
-  // })
-
-
-
-
-  // padInner.contents().on("click", ".embedRemoteImageSpan", function(e){
-  //   var url = $(this).data("url")
-  //   var id = $(this).attr("id")
-  //   var selector = "#"+id
-  //   var ace = padeditor.ace;
-
-  //   padeditor.ace.callWithAce(function (aceTop) {
-  //     var repArr = aceTop.ace_getRepFromSelector(selector, padInner);
-  //     $.each(repArr, function(index, rep){
-  //       ace.callWithAce(function (ace){
-  //         ace.ace_performSelectionChange(rep[0],rep[1],true);
-  //         ace.ace_setAttributeOnSelection('insertEmbedPicture', 'embedRemoteImageSpanLarge:'+url);
-
-  //       });
-  //     });
-  //   }, "changeEmbedPicture");
-
-  // })
+    $.gritter.add({
+      'title': 'selected image url',
+      'text': url,
+      'sticky': true,
+      'class_name': 'info'
+    });
+  });
+  return cb();
 }
--- plugins-available/ep_insert_media/static/js/main.js
+++ plugins-available/ep_insert_media/static/js/main.js
@@ -1,30 +1,23 @@
 var $ = require('ep_etherpad-lite/static/js/rjquery').$;
 
 $(document).ready(function () {
-  var first =true;
- 
-  $(document).on('click', '.btnMediaSize', function() {
-      $('.btnMediaSize').removeClass('on');
-      $(this).addClass('on');
-      $("#selectedSize").val($(this).text())
+
+  $(document).on('click', '.btnMediaSize', function () {
+    $('.btnMediaSize').removeClass('on');
+    $(this).addClass('on');
+    $("#selectedSize").val($(this).text())
   });
 
-  $(document).on('click', '.btnAlignDirection', function() {
+  $(document).on('click', '.btnAlignDirection', function () {
     $('.btnAlignDirection').removeClass('on');
     $(this).addClass('on');
     $("#selectedAlign").val($(this).data("value"))
   });
-  
-
-  $("#file").change(function(){
-    $("#embedMediaSrc").val("")
-  })
 
   $("#insertEmbedMedia").click(function () {
     // Can not use this yet, fix in main etherpad
     // padeditbar.toogleDropDown("embedMediaModal");
     var module = $("#embedMediaModal");
-    console.log(module)
     if (!module.hasClass('insertEmbedMedia-show')) {
       module.addClass("insertEmbedMedia-show");
     } else {
@@ -34,187 +27,62 @@ $(document).ready(function () {
 
   $("#doEmbedMedia").click(function () {
     var padeditor = require('ep_etherpad-lite/static/js/pad_editor').padeditor;
-    //console.log($("#mediaSizeSelect btnMediaSize on").text(),"wwwwwwwwwwwww")
-    
+
     //$("#embedMediaModal").slideUp("fast");
     $("#embedMediaModal").removeClass("insertEmbedMedia-show");
-    var url =$("#embedMediaSrc")[0].value
-    var imageSize =$("#selectedSize").val()
-    var imageAlign =$("#selectedAlign").val()
-    var imageUrl = escape(url) ;
+    var url = $("#embedMediaSrc")[0].value
+    var imageSize = $("#selectedSize").val()
+    var imageAlign = $("#selectedAlign").val()
+    var imageUrl = escape(url);
     var mediaData = {
-      "url" : imageUrl,
-      "align" : imageAlign,
-      "size" : imageSize
+      "url": imageUrl,
+      "align": imageAlign,
+      "size": imageSize
     }
-     if(url!=""){
-      if (url.indexOf('http://') == 0 || url.indexOf('https://') == 0) {
-        if (url.indexOf("www.youtube.com") != -1 || url.indexOf("youtu.be") != -1 || url.indexOf("vimeo.com") != -1) {
-          return padeditor.ace.callWithAce(function (ace) {
-            var rep = ace.ace_getRep();
-            ace.ace_replaceRange(rep.selStart, rep.selEnd, "E");
-            ace.ace_performSelectionChange([rep.selStart[0],rep.selStart[1]-1], rep.selStart, false);
-            ace.ace_performDocumentApplyAttributesToRange(rep.selStart, rep.selEnd, [["embedMedia", JSON.stringify(mediaData)]]);
-          }, "embedMedia");
-        }else{
-          var img = new Image();
-        
-          img.onload = function(){
-            return padeditor.ace.callWithAce(function (ace) {
-              var rep = ace.ace_getRep();
-              ace.ace_replaceRange(rep.selStart, rep.selEnd, "E");
-              ace.ace_performSelectionChange([rep.selStart[0],rep.selStart[1]-1], rep.selStart, false);
-              ace.ace_performDocumentApplyAttributesToRange(rep.selStart, rep.selEnd, [["insertEmbedPicture", JSON.stringify(mediaData)]]);
-            }, "insertEmbedPicture");
-          }  
-          };
-          img.onerror = function(){
-              if (!$("#editorcontainerbox").hasClass("flex-layout")) {
-                $.gritter.add({
-                  'title': 'Error',
-                  'text': 'ep_insert_media: image is not supported.',
-                  'sticky': true,
-                  'class_name': 'error'
-                });
-              }
-          }
-        
-         img.src = url;
-       
+
+    if (isValidImageURL(url)) {
+      var img = new Image();
+
+      img.onload = function () {
+        return padeditor.ace.callWithAce(function (ace) {
+          var rep = ace.ace_getRep();
+          ace.ace_replaceRange(rep.selStart, rep.selEnd, "E");
+          ace.ace_performSelectionChange([rep.selStart[0], rep.selStart[1] - 1], rep.selStart, false);
+          ace.ace_performDocumentApplyAttributesToRange(rep.selStart, rep.selEnd, [["insertEmbedPicture", JSON.stringify(mediaData)]]);
+        }, "insertEmbedPicture");
       }
 
-      $("#embedMediaSrc").val("")
+      img.onerror = function () {
+        if (!$("#editorcontainerbox").hasClass("flex-layout")) {
+          $.gritter.add({
+            'title': 'Error',
+            'text': 'ep_insert_media: image is not supported.',
+            'sticky': true,
+            'class_name': 'error'
+          });
+        }
+      }
 
-    }else{
-      uploadAction(mediaData)
+      img.src = url;
+      $("#embedMediaSrc").val("")
+    } else {
+      alert("Invalid URL");
     }
-    
   });
 
   $("#cancelEmbedMedia").click(function () {
     //$("#embedMediaModal").slideUp("fast");
     $("#embedMediaModal").removeClass("insertEmbedMedia-show");
   });
-
-
 })
 
-  function uploadAction(mediaData){
-    var padeditor = require('ep_etherpad-lite/static/js/pad_editor').padeditor;
-
-    var fd = new FormData();
-    var files = $('#file')[0].files[0];
-    fd.append('file',files);
-    $.ajax({
-        url: '/p/' + clientVars.padId + '/pluginfw/ep_insert_media/upload',
-        type: 'post',
-        data: fd,
-        contentType: false,
-        processData: false,
-        success: function(response){
-            if(response&&response.error==false ){
-
-                if (isImage(response.fileType)){
-                  if (response.type =="s3")
-                    var image_url ='/p/getImage/'+response.fileName
-                  else
-                    var image_url =response.fileName
-                  mediaData.url =  escape(image_url) ;
-                  padeditor.ace.callWithAce(function (ace) {
-                    var rep = ace.ace_getRep();
-                    ace.ace_replaceRange(rep.selStart, rep.selEnd, "E");
-                    ace.ace_performSelectionChange([rep.selStart[0],rep.selStart[1]-1], rep.selStart, false);
-                    ace.ace_performDocumentApplyAttributesToRange(rep.selStart, rep.selEnd, [["insertEmbedPicture", JSON.stringify(mediaData)]]);
-                  }, "insertEmbedPicture");
-                }if (isVideo(response.fileType)){
-                  if (response.type =="s3")
-                    var video_url ='/p/getVideo/'+response.fileName
-                  else
-                    var video_url =response.fileName
-                    mediaData.url =  escape(video_url) ;
-
-                  padeditor.ace.callWithAce(function (ace) {
-                    var rep = ace.ace_getRep();
-                    ace.ace_replaceRange(rep.selStart, rep.selEnd, "E");
-                    ace.ace_performSelectionChange([rep.selStart[0],rep.selStart[1]-1], rep.selStart, false);
-                    ace.ace_performDocumentApplyAttributesToRange(rep.selStart, rep.selEnd, [["insertEmbedVideo", JSON.stringify(mediaData)]]);
-                  }, "insertEmbedVideo");
-                }if (isAudio(response.fileType)){
-                  if (response.type =="s3")
-                    var audio_url ='/p/getMedia/'+response.fileName
-                  else
-                    var audio_url =response.fileName
-                    mediaData.url =  escape(audio_url) ;
-
-                  padeditor.ace.callWithAce(function (ace) {
-                    var rep = ace.ace_getRep();
-                    ace.ace_replaceRange(rep.selStart, rep.selEnd, "E");
-                    ace.ace_performSelectionChange([rep.selStart[0],rep.selStart[1]-1], rep.selStart, false);
-                    ace.ace_performDocumentApplyAttributesToRange(rep.selStart, rep.selEnd, [["insertEmbedAudio", JSON.stringify(mediaData)]]);
-                  }, "insertEmbedAudio");
-                }
-              
-               
-                
-                $("#embedMediaModal").removeClass("insertEmbedMedia-show");
-                $("#file").val("")
-            }else{
-                $.gritter.add({
-                  'title': 'Error',
-                  'text': 'ep_insert_media: '+response.error,
-                  'sticky': true,
-                  'class_name': 'error'
-                });
-            }
-        },
-    });
-  } 
-
-
-
-
- 
-
-function isImage (filename) {
-  switch (filename.toLowerCase()) {
-    case '.jpg':
-    case '.gif':
-    case '.bmp':
-    case '.png':
-    case '.jpeg':
-
-      //etc
-      return true;
+function isValidImageURL(url) {
+  if (!url) return false;
+  if (url.startsWith("http://") || url.startsWith("https://")) {
+    const urlParts = url.split(".");
+    const fileExtension = urlParts[urlParts.length - 1];
+    return ["jpg", 'jpeg', 'bmp', 'png', 'gif'].includes(fileExtension);
+  } else {
+    return false;
   }
-  return false;
-}
-
-function isVideo (filename) {
-  switch (filename.toLowerCase()) {
-    case '.m4v':
-    case '.avi':
-    case '.mpg':
-    case '.mp4':
-    case '.webm':
-
-      // etc
-      return true;
-  }
-  return false;
-}
-
-function isAudio (filename) {
-  switch (filename.toLowerCase()) {
-    case '.mp3':
-    case '.ogg':
-    case '.m4a':
-    case '.flac':
-    case '.wav':
-    case '.wma':
-    case '.aac':
-
-      // etc
-      return true;
-  }
-  return false;
 }
