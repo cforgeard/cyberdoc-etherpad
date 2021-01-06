df9bc4030bdf323849adab2b0bed6fd325c2cbb8
Clément FORGEARD <clement_forgeard@outlook.fr>
2021-01-06 06:48:13 +0000

    [core] disable update check

--- src/node/utils/UpdateCheck.js
+++ src/node/utils/UpdateCheck.js
@@ -20,25 +20,28 @@ function loadEtherpadInformations() {
 }
 
 exports.getLatestVersion = function () {
-  exports.needsUpdate();
-  return infos.latestVersion;
+  /*exports.needsUpdate();
+  return infos.latestVersion;*/
+  return settings.getEpVersion();
 };
 
 exports.needsUpdate = function (cb) {
-  loadEtherpadInformations().then((info) => {
+  /*loadEtherpadInformations().then((info) => {
     if (semver.gt(info.latestVersion, settings.getEpVersion())) {
       if (cb) return cb(true);
     }
   }).catch((err) => {
     console.error(`Can not perform Etherpad update check: ${err}`);
     if (cb) return cb(false);
-  });
+  });*/
+  if (cb) return cb(false);
 };
 
 exports.check = function () {
-  exports.needsUpdate((needsUpdate) => {
+  console.warn("[!] update checking disabled");
+  /*exports.needsUpdate((needsUpdate) => {
     if (needsUpdate) {
       console.warn(`Update available: Download the actual version ${infos.latestVersion}`);
     }
-  });
+  });*/
 };
