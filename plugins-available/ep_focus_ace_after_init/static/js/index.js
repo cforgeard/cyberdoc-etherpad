9f335dc9d564e64efe0785deb5f5dcb780b309fe
Clément FORGEARD <clement_forgeard@outlook.fr>
2021-01-10 10:52:52 +0000

    [ep_focus_ace_after_init] fix bug

--- plugins-available/ep_focus_ace_after_init/static/js/index.js
+++ plugins-available/ep_focus_ace_after_init/static/js/index.js
@@ -1,6 +1,6 @@
 exports.postAceInit = function(hookName, args, cb) {
     const aceOuterIframe = document.querySelector('iframe[name="ace_outer"]');
     const aceInnerIframe = aceOuterIframe.contentWindow.document.querySelector('iframe[name="ace_inner"]');
-    aceInnerIframe.contentDocument.getElementById("magicdomid1").click();
+    aceInnerIframe.contentDocument.getElementById("innerdocbody").firstElementChild.click();
     return cb();
 }
